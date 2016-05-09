a37529b10e38af338de8a2fd4e5e7dbaceecaf5b
wuyinlei <wuyinlei19931118@hotmail.com>
2016-05-09 22:38:55 +0800

    最初级服务器项目搭建，302重定向

---
+++ src/com/yinlei/MyServlet.java
@@ -0,0 +1,32 @@
+package com.yinlei;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MyServlet
+ */
+@WebServlet("/MyServlet")
+public class MyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public MyServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Œ“¿¥¡À");
+		response.setStatus(302);
+		response.setHeader("Location", "/Test01/ServletDemo");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ src/com/yinlei/ServletDemo2.java
@@ -0,0 +1,53 @@
+package com.yinlei;
+
+import java.io.IOException;
+import java.util.zip.GZIPOutputStream;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.tomcat.util.http.fileupload.ByteArrayOutputStream;
+
+/**
+ * Servlet implementation class ServletDemo2
+ */
+@WebServlet("/ServletDemo2")
+public class ServletDemo2 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public ServletDemo2() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String s = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+				+ "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+				+ "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+				+ "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+		
+		byte[] bs = s.getBytes();
+		System.out.println("压缩前的长度："+bs.length);
+		//创建一个字节数组输出流
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		//希望用gzip压缩一下发送
+		//创建一个压缩流，包装了输出流
+		GZIPOutputStream gzip = new GZIPOutputStream(baos);
+		//将数据进行压缩
+		gzip.write(bs);
+		gzip.close();
+		bs = baos.toByteArray();  //压缩后的数据
+		System.out.println(bs.length);
+		response.getOutputStream().write(bs);
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
